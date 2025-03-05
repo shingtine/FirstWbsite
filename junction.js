93fa7dd51aec207678f84d8fb5d16bc753210d03
shingtine <tintinabarera@gmail.com>
2025-03-05 11:02:22 +0800

    update

---
+++ junction.js
@@ -0,0 +1,11 @@
+var menu=document.querySelector('.menu');
+var menuBtn=document.querySelector('.menu-btn');
+var closeBtn=document.querySelector('.close-btn');
+
+menuBtn.addEventListener("click",()=>{
+  menu.classList.add('active');
+});
+
+closwBtn.AddEventListener("click",()=>{
+  meny.classList.remove('active');
+});
